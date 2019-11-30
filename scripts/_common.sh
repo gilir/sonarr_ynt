3ef684a6354206bbf41a05e77fd19dfbfcb1c67e
Julien Lavergne <julien@lavergne.online>
2019-12-01 00:54:15 +0100

    Add mono-devel to depends

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,8 @@
 #=================================================
 DATADIR="/home/$app"
 # dependencies used by the app
-pkg_dependencies="libsqlite3-0 libmediainfo0v5 mono-runtime ca-certificates-mono libmono-system-net-http4.0-cil"
+#TODO Do better than mono-devel
+pkg_dependencies="libsqlite3-0 libmediainfo0v5 mono-runtime ca-certificates-mono libmono-system-net-http4.0-cil mono-devel"
 
 #=================================================
 # PERSONAL HELPERS
