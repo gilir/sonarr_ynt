bfbf3038bedfa01d9ab4032df6a42a463c3fa3f9
Julien Lavergne <julien@lavergne.online>
2019-12-02 00:47:19 +0100

    Fix confdir variable

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,6 +7,7 @@ DATADIR="/home/$app"
 # dependencies used by the app
 #TODO Do better than mono-devel
 pkg_dependencies="libsqlite3-0 libmediainfo0v5 mono-runtime ca-certificates-mono libmono-system-net-http4.0-cil mono-devel"
+configdir=""
 
 #=================================================
 # PERSONAL HELPERS
